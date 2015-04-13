11f382c6c86b55ecd011c00d39581a91e43f2fec
Vutov <VutovSpas@gmail.com>
2015-04-13 19:48:49 +0300

    html changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller {
 	 */
 	public function store()
 	{
-		//
+		echo 'submited info here';
 	}
 
 	/**
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,11 +2,21 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>Master Page</title>
+    <style>
+        header, footer {
+            border: 1px solid #000000;
+        }
+
+        main {
+            margin: 20px 0;
+            border: 1px solid red;
+        }
+    </style>
 </head>
 <body>
 <div class="wrapper">
     <header>
-        <h1>Header</h1>
+        <h5>Header</h5>
         <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/forum">Forum</a></li>
@@ -19,7 +29,14 @@
         @yield('content')
     </main>
     <footer>
-        <h5><div>Footer</div>Lorem ipsum dolor sit amet, quis dui pellentesque nullam aliquam dignissim, mattis ut ac sapien neque metus in, ac gravida scelerisque adipiscing. Primis condimentum semper lectus mauris ut vitae, eveniet ornare convallis quisque leo gravida, vitae est quis, nec imperdiet, ac ultricies sed eget laoreet vestibulum. In consectetuer vitae ut ante litora dolor, cras dui velit etiam fermentum fringilla ligula, sed metus molestie fringilla facilisis amet. Sed tortor ac eg</h5>
+        <h5>
+            <div>Footer</div>
+            Lorem ipsum dolor sit amet, quis dui pellentesque nullam aliquam dignissim, mattis ut ac sapien neque metus
+            in, ac gravida scelerisque adipiscing. Primis condimentum semper lectus mauris ut vitae, eveniet ornare
+            convallis quisque leo gravida, vitae est quis, nec imperdiet, ac ultricies sed eget laoreet vestibulum. In
+            consectetuer vitae ut ante litora dolor, cras dui velit etiam fermentum fringilla ligula, sed metus molestie
+            fringilla facilisis amet. Sed tortor ac eg
+        </h5>
     </footer>
 </div>
 </body>
