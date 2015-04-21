b6bc79a44caec4550992e0756467ebcffc80cf23
Spas Vutov <VutovSpas@gmail.com>
2015-04-21 15:50:16 +0300

    Added 404 view

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="panel panel-default">
+        <h1 class="alert alert-warning" role="alert">
+            <strong>Sorry</strong>, the page you are looking for could not be found.
+        </h1>
+        <div class="panel-body">
+            <a href="/" class="btn btn-primary">Return Home</a>
+        </div>
+    </div>
+
+@endsection
